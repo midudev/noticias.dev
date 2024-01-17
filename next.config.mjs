dbd9afcd8992e8b0f54b41dfcc6b695736dd1ceb
Miguel Ángel Durán <miduga@gmail.com>
2024-01-17 13:06:14 +0100

    Disable outputFileTracing

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,7 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracing: false,
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   experimental: {
